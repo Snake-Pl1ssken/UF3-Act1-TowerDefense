2a8f8a7870c65cf4cc4c2e334e8290ac44a74ed0
Snake-Pl1ssken <jorgeluisgarciaorbegoso@gmail.com>
2025-03-20 11:35:10 +0100

    casi finalizado el ejercicio 1

---
+++ Assets/System/Oleadas/Scripts/DefinicionOleada.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DefinicionOleada : ScriptableObject
+{
+    [System.Serializable]
+
+    public class BloqueEnemigos
+    {
+        public GameObject tipoEnemigos;
+        public int cantidad;
+        public float enemigosPorSegundo;
+    }
+    public BloqueEnemigos[] bloques;
+}
